3b7c1b301922f1306c1a78b0b3666db831157da7
Mohamad Aid Bacuk Zanab <mohammedsharif067@gmail.com>
2023-05-18 22:44:13 +0300

    Done Assignment

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Write your solution in this file!
+var customerName = 'bob';
+function upperCaseCustomerName (){
+    return customerName= customerName.toUpperCase();
+}
+
+var setBestCustomer = () => bestCustomer = "not bob";
+
+
+
+  function overwriteBestCustomer(){
+    bestCustomer = "maybe bob";
+}
+
+const leastFavoriteCustomer = "Mohamad";
+
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = "Nour"; 
+}
